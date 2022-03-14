934c1b04afadbb7cdd1894770d43e857f15a9c65
Emre Özdeveci <emre.ozdeveci@MAC-IT-47.home>
2022-03-14 23:44:22 +0300

    Impementing the Transport Package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/eozdeveci/CommentRestApi/internal/transport/http"
+)
 
 type App struct{}
 
 func (app *App) Run() error {
 	fmt.Println("Setting up APP")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
 	return nil
 }
 
