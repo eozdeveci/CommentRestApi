3a3ba3a91a02a880d18ecc0418a9bac6de7aff13
Emre Özdeveci <emre.ozdeveci@MAC-IT-47.home>
2022-03-14 23:27:15 +0300

    Structuring application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up APP")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go - Comment Rest Api")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up Rest Api")
+		fmt.Println(err)
+	}
 }
